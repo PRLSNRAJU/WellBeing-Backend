4f4352768adf0100f9ce828be66b290d6eebe46d
rlsnrajupusapati <rlsnrajupusapati@gmail.com>
2021-10-20 11:22:20 +0530

    added common projects

---
+++ Common.Dto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common.Dto
+{
+    public class UserDto
+    {
+        public string ID;
+        public string PHONENumber;
+        public string Name;
+        public string BirthDay;
+        public string BDayConsent;
+    }
+}
